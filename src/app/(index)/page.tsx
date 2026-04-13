0fafb1ee3a3c87c27312f9ce444eb6be411f0375
jiayuding <17708215126@163.com>
2026-04-13 22:02:55 +0800

    feat: initial commit

---
+++ src/app/(index)/page.tsx
@@ -0,0 +1,15 @@
+// import Sort from "@/components/Sort"
+// import Products from "@/components/Products"
+// import { productsAction } from "@/actions/products"
+export default async function Page() {
+  // const res = await productsAction()
+  return (
+    <div>
+      hello Page
+    </div>
+    // <div className="container flex py-6">
+    //   <Sort />
+    //   <Products data={res.data} />      
+    // </div>
+  )
+}
--- src/app/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function Home() {
-  return (
-    <div>hello page 01</div>
-  );
-}
